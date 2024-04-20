5497e883d3604348272ee4ddec58f3884cf26ec8
Tomáš Gabriel <thomas2654cz@gmail.com>
2024-04-20 18:23:56 +0200

    Restyled BoxBase

--- Client/src/BoxBase.jsx
+++ Client/src/BoxBase.jsx
@@ -5,7 +5,10 @@ function BoxBase({ children }) {
     return (
         <Box
             bg="dark.5"
-            style={{ borderRadius: 15 }}
+            style={{ 
+                border: "1px solid var(--mantine-color-dark-4)",
+                borderRadius: "20px"
+            }}
         >
             {children}
         </Box>
